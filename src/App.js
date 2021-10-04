0e9d6ff10b71f95cad36d02aa27f0b6d6f993c6c
osh <dhtpgus7@gmail.com>
2021-10-04 23:20:09 +0900

    loadable-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 
 import Header from './components/Header';
-import AboutMe from './components/AboutMe';
+// import AboutMe from './components/AboutMe';
 import Works from './components/Works';
 import Footer from './components/Footer';
 
@@ -9,10 +9,18 @@ import GlobalStyle from './theme/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme/theme';
 
+import loadable from 'loadable-components';
+
 function App() {
   const aboutMeRef = useRef(null)
   const worksRef = useRef(null)
   const footerRef = useRef(null)
+
+  debugger
+  const Header = loadable(() => import('./components/Header'))
+  const AboutMe = loadable(() => import('./components/AboutMe'))
+  const Works = loadable(() => import('./components/Works'))
+  const Footer = loadable(() => import('./components/Footer'))
 
   return (
     <>
