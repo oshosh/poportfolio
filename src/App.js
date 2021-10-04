3b4d36fee84d8abe1eaa1991f44940876d049a12
osh <dhtpgus7@gmail.com>
2021-10-04 23:47:06 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { useRef } from 'react';
 
-import Header from './components/Header';
-// import AboutMe from './components/AboutMe';
-import Works from './components/Works';
-import Footer from './components/Footer';
-
 import GlobalStyle from './theme/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme/theme';
@@ -16,7 +11,6 @@ function App() {
   const worksRef = useRef(null)
   const footerRef = useRef(null)
 
-  debugger
   const Header = loadable(() => import('./components/Header'))
   const AboutMe = loadable(() => import('./components/AboutMe'))
   const Works = loadable(() => import('./components/Works'))
