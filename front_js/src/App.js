42aebdb6678030fbaed20cc7f4d493b3c921578d
osh <dhtpgus7@gmail.com>
2022-12-27 18:15:45 +0900

    no message

--- front_js/src/App.js
+++ front_js/src/App.js
@@ -17,7 +17,7 @@ function App() {
   const AboutMe = loadable(() => import("./components/AboutMe"));
   const Works = loadable(() => import("./components/Works"));
   const Footer = loadable(() => import("./components/Footer"));
-  console.log('test')
+  console.log('test12412')
 
   return (
     <>
