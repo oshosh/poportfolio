94368c92ba924e6bc24268ade8451cd51ba11576
oshosh <dhtpgus7@gmail.com>
2022-12-28 00:37:36 +0900

    no message

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,47 @@
+const { whenDev } = require('@craco/craco')
+const path = require('path')
+
 module.exports = {
   babel: {
     presets: [
       [
         '@babel/preset-env',
         {
+          modules: false,
           useBuiltIns: 'entry',
-          corejs: 3,
-          // Other options such as targets, modules, etc. can also be specified here.
+          corejs: {
+            version: 3,
+            proposals: true
+          }
         },
       ],
     ],
+  },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      paths.appBuild = 'dist'
+      
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        ...{
+          filename: whenDev(() => 'static/js/bundle.js', 'static/js/[name].js'),
+          chunkFilename: 'static/js/[name].js'
+        },
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+      }
+
+      webpackConfig.devtool = false
+
+      webpackConfig.optimization.splitChunks = {
+        ...webpackConfig.optimization.splitChunks,
+        ...{
+          chunks: 'all',
+          name: true
+        }
+      }
+
+      return webpackConfig
+    }
   },
 };
